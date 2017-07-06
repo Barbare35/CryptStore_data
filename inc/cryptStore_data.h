49866c1cc19ac637271d175c7ad5e252e3da4c6a
Michael barbarin <michael.barbarin@gmail.com>
2017-07-06 10:32:37 +0200

    that compile. Fist step is ok. Options is create. I manage now by feature for next

---
+++ inc/cryptStore_data.h
@@ -0,0 +1,13 @@
+#ifndef CRYPTSTORE_DATA_H
+# define CRYPTSTORE_DATA_H
+
+# include <stdbool.h>
+
+struct valoption
+{
+	char	opt;
+	_Bool	val;
+	char	*arg;
+};
+
+#endif
--- src/main.c
+++ src/main.c
@@ -5,6 +5,14 @@
 #include <errno.h>
 #include <string.h>
 
+#include "cryptStore_data.h"
+
+void process(struct valoption val[], char *addfile, char *safe)
+{
+	(void)val, (void)addfile, (void)safe;
+	return ;
+}
+
 int usage(char *av, struct option options[])
 {
 	int i = 0;
@@ -12,20 +20,30 @@ int usage(char *av, struct option options[])
 	dprintf(STDERR_FILENO, "Usage: %s [", av);
 	while (options[i].name != NULL)
 	{
-		dprintf(STDERR_FILENO, "%s", options[i++].name);
+		dprintf(STDERR_FILENO, "%s%s", options[i].name, \
+				options[i].has_arg == required_argument ? " <file>" : "");
+		++i;
 		if (options[i].name != NULL)
 			dprintf(STDERR_FILENO, "|");
 	}
-	dprintf(STDERR_FILENO, "] addfile safe");
+	dprintf(STDERR_FILENO, "] addfile safe\n");
 	return (-1);
 }
 
 int main(int ac, char **av)
 {
-	FILE *safe; //safe that contain all file
 	FILE *file;
 	int opt;
+	struct valoption valopt[] = {
+		{'h', false, NULL},
+		{'d', false, NULL},
+		{'l', false, NULL},
+		{'v', false, NULL},
+		{'o', false, NULL},
+		{0, 0, 0},
+	};
 	struct option options[] = {
+		{"help",		no_argument,       	0, 'h'},
 		{"debug",		no_argument,       	0, 'd'},
 		{"list-file",	no_argument,		0, 'l'},
 		{"verbose",		no_argument,		0, 'v'},
@@ -33,22 +51,41 @@ int main(int ac, char **av)
 		{0, 0, 0, 0}
 	};
 
-	if (ac < 2)
+	if (ac < 3) //minima addfile and safefilename
 		return(usage(av[0], options));
-	if ((file = fopen(av[ac - 2], "r")) < 0)
+	while (1)
 	{
-		dprintf(STDERR_FILENO, "addfile %s", strerror(errno));
-		return (-1);
+		opt = getopt_long(ac, av, "o:d:l:v:c:", options, NULL);
+		if (opt == -1)
+			break;
+		switch (opt)
+		{
+			case 'd':
+				valopt[1].val = true;
+				break;
+			case 'h':
+				return (usage(av[0], options));
+			case 'l':
+				// TODO Add list-file function
+				return (0);
+			case 'v':
+				valopt[3].val = true;
+				break;
+			case 'o':
+				if ((ac - 2) <= 2)
+					return (usage(av[0], options));
+				valopt[4].val = true;
+				valopt[4].arg = optarg;
+				break;
+			default:
+				return (usage(av[0], options));
+		}
 	}
-	else if ((safe = fopen(av[ac - 1], "rw")) < 0)
+	if ((file = fopen(av[ac - 2], "r")) == NULL)
 	{
-		dprintf(STDERR_FILENO, "safe %s", strerror(errno));
+		dprintf(STDERR_FILENO, "%s %s", av[ac - 2], strerror(errno));
 		return (-2);
 	}
-	else if ((opt = getopt_long(ac, av, "o:d:l:v:c:", options, NULL)) < 0)
-	{
-		dprintf(STDERR_FILENO, "options %s", strerror(errno));
-		return (-3);
-	}
+	process(valopt, av[ac - 2], av[ac - 1]);
 	return (0);
 }
